f8a19fb9a4677fb21473b093b52b2f24bd75439f
Vishal Pathak <vishalpathak.er@gmail.com>
2025-06-13 09:51:40 +0530

    project initialise

---
+++ eslint.config.js
@@ -0,0 +1,31 @@
+import eslintPluginTs from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+
+/** @type {import("eslint").Linter.FlatConfig[]} */
+export default [
+  {
+    files: ['**/*.ts'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module'
+      }
+    },
+    plugins: {
+      '@typescript-eslint': eslintPluginTs
+    },
+    rules: {
+      'no-unused-vars': 'warn',
+      'no-console': 'off',
+      'semi': ['error', 'always'],
+      'quotes': ['error', 'single'],
+      '@typescript-eslint/no-explicit-any': 'off'
+    }
+  },
+  {
+    rules: {
+      // For any JS files (optional)
+    }
+  }
+];
