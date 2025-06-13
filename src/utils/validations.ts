5b337fb9ce1eaa017fe4d2348a79d1a9333e3654
Vishal Pathak <vishalpathak.er@gmail.com>
2025-06-13 16:10:43 +0530

    fix validation

--- src/utils/validations.ts
+++ src/utils/validations.ts
@@ -25,23 +25,16 @@ export const registerValidations = [
     .isEmail()
     .withMessage('Enter Valid Email')
     .custom(async (value) => {
-      try {
         const exists = await User.exists({ email: value });
         if (exists) throw new Error('Email already registered');
-      } catch (err) {
-        logger.error("Error in Email Validation during register " , err);
-        throw new Error('Internal Server Error');
-      }
       return true;
     }),
-
   body('password')
     .trim()
     .notEmpty()
     .withMessage('Password is required')
     .isLength({ min: 8, max: 64 })
     .withMessage('Password must be between 8 and 64 characters'),
-
   body('role')
     .optional()
     .isString()
