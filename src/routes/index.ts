c99f4122449bb7bd9a3425ce23579f154f15682a
Vishal Pathak <vishalpathak.er@gmail.com>
2025-06-13 16:11:43 +0530

    add a health check api endpoint

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -8,6 +8,13 @@ import userRouter from "@/routes/v1/user";
 import blogRouter from "@/routes/v1/blog";
 
 const router = Router();
+router.get('/' , (_req , res) => {
+    res.status(200).json({
+        status : "ok",
+        message : "API is running",
+        timeStamp : new Date().toLocaleDateString()
+    })
+})
 
 // Auth Router 
 router.use('/auth' , authRouter);
