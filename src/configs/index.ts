e78b482983c327e14d706773669d5e442fa2ca0a
Vishal Pathak <vishalpathak.er@gmail.com>
2025-06-13 16:13:26 +0530

    add jwt configs

--- src/configs/index.ts
+++ src/configs/index.ts
@@ -1,6 +1,7 @@
 
 // node_modules
 import { configDotenv } from "dotenv";
+import type ms from "ms"
 
 configDotenv();
 
@@ -12,6 +13,11 @@ const config = {
     MONGO_URI : process.env.MONGO_URI,
     APP_NAME : process.env.APP_NAME || "BlogKaro",
     DB_NAME : process.env.DB_NAME || "blogkaro",
+    WHITELIST_EMAILS : process.env.WHITELIST_EMAILS!,
+    JWT_ACCESS_TOKEN_SECRET: process.env.JWT_ACCESS_TOKEN_SECRET!,
+    JWT_REFRESH_TOKEN_SECRET: process.env.JWT_REFRESH_TOKEN_SECRET!,
+    JWT_ACCESS_TOKEN_EXPIRATION: process.env.JWT_ACCESS_TOKEN_EXPIRATION as ms.StringValue ,
+    JWT_REFRESH_TOKEN_EXPIRATION: process.env.JWT_REFRESH_TOKEN_EXPIRATION as ms.StringValue,
     
 }
 
