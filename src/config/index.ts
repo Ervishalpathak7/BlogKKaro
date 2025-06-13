f2800ef11a5a0f80985b22a96311a0dc43fd9209
Vishal Pathak <vishalpathak.er@gmail.com>
2025-06-13 11:55:16 +0530

    add cors configuration

--- src/config/index.ts
+++ src/config/index.ts
@@ -8,6 +8,7 @@ const config = {
     PORT : process.env.PORT || 3000,
     LOG_LEVEL : process.env.LOG_LEVEL || 'info',
     ENV : process.env.NODE_ENV || 'development',
+    WHITELIST_OGIGIN : process.env.WHITET_ORIGIN!,
 }
 
 export default config;
--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,39 @@
+// node_modules
+import express from 'express';
+import cors, { CorsOptions } from 'cors';
 
-// import node modules 
-import express from "express";
+// custom_modules
+import config from '@/config';
+import logger from '@/config/winston';
 
-
+// initialise the app with express
 const app = express();
 
-( async () => {
-    try {
-        // Connect Database 
-
-        app.listen('3000' , () => {
-            console.log("server is running on port 3000");
-        })
-
-    } catch (error) {
+// cors configuration
+const corsOptions: CorsOptions = {
+  origin(origin, callback) {
+    if (
+      config.ENV === 'development' ||
+      !origin ||
+      config.WHITELIST_OGIGIN.includes(origin)
+    ) {
+      callback(null, true);
+    } else {
+        callback(new Error(`Cors not allowed origin : ${origin}`) , false);
+        logger.warn(`Cors not allowed origin : ${origin}`);
     }
-})();
+  },
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true
+}
 
+app.use(cors(corsOptions));
+
+(async () => {
+  try {
+    app.listen(config.PORT, () => {
+      logger.info(`Server started running on http://localhost:${config.PORT}`);
+    });
+  } catch (error) {}
+})();
