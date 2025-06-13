7330f06d9cc9585e202a3328d4fd984c558159b2
Vishal Pathak <vishalpathak.er@gmail.com>
2025-06-13 16:12:09 +0530

    fix validation middleware

--- src/middlewares/validation.ts
+++ src/middlewares/validation.ts
@@ -8,19 +8,10 @@ const validationMiddleware = (
 ) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-    const formattedErrors = errors
-      .array()
-      .reduce<Record<string, string>>((acc, err) => {
-        if ('param' in err && 'msg' in err) {
-          acc[(err as { param: string }).param] = (err as { msg: string }).msg;
-        }
-        return acc;
-      }, {});
-
-     res.status(400).json({
+    res.status(400).json({
       status: 'error',
-      message: 'Validation failed',
-      errors: formattedErrors,
+      message: errors.array()[0].msg,
+      errors: errors.mapped(),
     });
     return;
   }
