c83e0d08dac7d458d713ade145724b200319850f
Vishal Pathak <vishalpathak.er@gmail.com>
2025-06-15 15:00:45 +0530

    add user routes

--- src/routes/v1/user.ts
+++ src/routes/v1/user.ts
@@ -1,14 +1,17 @@
+import deleteCurrentUserController from "@/controllers/v1/user/deleteCurrentUser";
+import getCurrentUserController from "@/controllers/v1/user/getCurrentUser";
+import updateCurrentUserController from "@/controllers/v1/user/updateCurrentUser";
 import { Router } from "express";
 
 const userRouter = Router();
 
 // get current user 
-userRouter.get('/me');
+userRouter.get('/me' , getCurrentUserController);
 
 // update current user 
-userRouter.put('/me');
+userRouter.put('/me' , updateCurrentUserController);
 
 // delete current user 
-userRouter.delete('/me');
+userRouter.delete('/me' , deleteCurrentUserController);
 
 export default userRouter;
