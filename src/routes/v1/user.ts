b9f0266cca80e6c2ac7066fa3900e45c4f5691d4
Vishal Pathak <vishalpathak.er@gmail.com>
2025-06-15 15:46:31 +0530

    add middlewares in user routes

--- src/routes/v1/user.ts
+++ src/routes/v1/user.ts
@@ -1,17 +1,38 @@
-import deleteCurrentUserController from "@/controllers/v1/user/deleteCurrentUser";
-import getCurrentUserController from "@/controllers/v1/user/getCurrentUser";
-import updateCurrentUserController from "@/controllers/v1/user/updateCurrentUser";
-import { Router } from "express";
+import deleteCurrentUserController from '@/controllers/v1/user/deleteCurrentUser';
+import getCurrentUserController from '@/controllers/v1/user/getCurrentUser';
+import updateCurrentUserController from '@/controllers/v1/user/updateCurrentUser';
+import authenticationMiddleware from '@/middlewares/authentication';
+import validationMiddleware from '@/middlewares/validation';
+import { authenticationValidation } from '@/utils/validations';
+import { Router } from 'express';
 
 const userRouter = Router();
 
-// get current user 
-userRouter.get('/me' , getCurrentUserController);
+// get current user al
+userRouter.get(
+  '/me',
+  authenticationValidation,
+  validationMiddleware,
+  authenticationMiddleware,
+  getCurrentUserController
+);
 
-// update current user 
-userRouter.put('/me' , updateCurrentUserController);
+// update current user
+userRouter.put(
+  '/me',
+  authenticationValidation,
+  validationMiddleware,
+  authenticationMiddleware,
+  updateCurrentUserController
+);
 
-// delete current user 
-userRouter.delete('/me' , deleteCurrentUserController);
+// delete current user
+userRouter.delete(
+  '/me',
+  authenticationValidation,
+  validationMiddleware,
+  authenticationMiddleware,
+  deleteCurrentUserController
+);
 
 export default userRouter;
