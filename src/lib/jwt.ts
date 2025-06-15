6f4aa7a5133ad91f1d3c6191ad4addbda8203a33
Vishal Pathak <vishalpathak.er@gmail.com>
2025-06-15 12:50:45 +0530

    update  token generation functions

--- src/lib/jwt.ts
+++ src/lib/jwt.ts
@@ -1,15 +1,14 @@
 import jwt from 'jsonwebtoken';
 import config from '@/configs';
-import { Types } from 'mongoose';
 
-export const generateAccessToken = (userId: Types.ObjectId): string => {
+export const generateAccessToken = (userId: string): string => {
   return jwt.sign({ userId }, config.JWT_ACCESS_TOKEN_SECRET, {
     expiresIn: config.JWT_ACCESS_TOKEN_EXPIRATION,
     subject: 'accessToken',
   });
 };
 
-export const generateRefreshToken = (userId: Types.ObjectId): string => {
+export const generateRefreshToken = (userId: string): string => {
   return jwt.sign({ userId }, config.JWT_REFRESH_TOKEN_SECRET, {
     expiresIn: config.JWT_REFRESH_TOKEN_EXPIRATION,
     subject: 'refreshToken',
@@ -17,9 +16,27 @@ export const generateRefreshToken = (userId: Types.ObjectId): string => {
 };
 
 export const verifyAccessToken = (token: string) => {
-  return jwt.verify(token, config.JWT_ACCESS_TOKEN_SECRET);
+  try {
+    const decoded = jwt.verify(
+      token,
+      config.JWT_ACCESS_TOKEN_SECRET
+    ) as jwt.JwtPayload;
+    return { userId: decoded.userId };
+  } catch (err) {
+    return null;
+  }
 };
 
-export const verifyRefreshToken = (token: string) => {
-  return jwt.verify(token, config.JWT_REFRESH_TOKEN_SECRET);
+export const verifyRefreshToken = (
+  token: string
+): { userId: string } | null => {
+  try {
+    const decoded = jwt.verify(
+      token,
+      config.JWT_REFRESH_TOKEN_SECRET
+    ) as jwt.JwtPayload;
+    return { userId: decoded.userId };
+  } catch (err) {
+    return null;
+  }
 };
