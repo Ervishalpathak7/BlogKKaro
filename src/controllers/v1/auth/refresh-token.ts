b7299684f5b8d58c50b98e9dc889715b0f730991
Vishal Pathak <vishalpathak.er@gmail.com>
2025-06-15 12:50:59 +0530

    add refresh token controller

--- src/controllers/v1/auth/refresh-token.ts
+++ src/controllers/v1/auth/refresh-token.ts
@@ -1,7 +1,51 @@
-import { Request, Response } from "express";
+import { generateAccessToken, verifyRefreshToken } from '@/lib/jwt';
+import logger from '@/lib/winston';
+import RefreshToken from '@/models/refresh-token';
+import { Request, Response } from 'express';
 
-const refreshTokenController = async (req : Request , res : Response) : Promise<void> => {
+const refreshTokenController = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  try {
+    const token = req.cookies.refreshToken;
+    const existingToken = await RefreshToken.exists({ token });
 
-}
+    if (!existingToken) {
+      res.status(404).json({
+        status: 'error',
+        message: 'Invalid or expired token. Please log in again.',
+        data: {},
+      });
+      return;
+    }
 
-export default refreshTokenController
+    const payload = verifyRefreshToken(token);
+    if (!payload) {
+      res.status(401).json({
+        status: 'error',
+        message: 'Invalid or expired token. Please log in again.',
+        data: {},
+      });
+      return;
+    }
+
+    const accessToken = generateAccessToken(payload.userId);
+    res.status(200).json({
+      status: 'ok',
+      message: 'User authorized successfully',
+      data: { accessToken },
+    });
+
+    logger.info('Access token refreshed', { userId: payload.userId });
+    
+  } catch (error) {
+    logger.error('error during refreshing token ', error);
+    res.status(500).json({
+      status: 'error',
+      message: 'Internal server error',
+    });
+  }
+};
+
+export default refreshTokenController;
