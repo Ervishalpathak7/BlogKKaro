f377fb60180816b315a5436eb2a27f5bb32313dc
Vishal Pathak <vishalpathak.er@gmail.com>
2025-06-15 13:41:49 +0530

    add logout route

--- src/routes/v1/auth.ts
+++ src/routes/v1/auth.ts
@@ -1,44 +1,51 @@
-import loginController from "@/controllers/v1/auth/login";
-import logoutController from "@/controllers/v1/auth/logout";
-import refreshTokenController from "@/controllers/v1/auth/refresh-token";
-import registerController from "@/controllers/v1/auth/register";
-import validationMiddleware from "@/middlewares/validation";
-import { loginValidation, refreshTokenValidation, registerValidations } from "@/utils/validations";
-import { Router } from "express";
+import loginController from '@/controllers/v1/auth/login';
+import logoutController from '@/controllers/v1/auth/logout';
+import refreshTokenController from '@/controllers/v1/auth/refresh-token';
+import registerController from '@/controllers/v1/auth/register';
+import authenticationMiddleware from '@/middlewares/authentication';
+import validationMiddleware from '@/middlewares/validation';
+import {
+  loginValidation,
+  logoutValidation,
+  refreshTokenValidation,
+  registerValidations,
+} from '@/utils/validations';
+import { Router } from 'express';
 
-// router 
+// router
 const authRouter = Router();
 
 // login route
 authRouter.post(
-    '/login', 
-    loginValidation,
-    validationMiddleware,
-    loginController
+  '/login',
+  loginValidation,
+  validationMiddleware,
+  loginController
 );
 
 // register route
 authRouter.post(
-    '/register',
-    registerValidations,
-    validationMiddleware,
-    registerController
+  '/register',
+  registerValidations,
+  validationMiddleware,
+  registerController
 );
 
 // refresh-token route
 authRouter.post(
-    '/refresh-token',
-    refreshTokenValidation,
-    validationMiddleware,
-    refreshTokenController
+  '/refresh-token',
+  refreshTokenValidation,
+  validationMiddleware,
+  refreshTokenController
 );
 
 // logout route
 authRouter.post(
-    '/logout',
-    validationMiddleware,
-    logoutController
+  '/logout',
+  logoutValidation,
+  validationMiddleware,
+  authenticationMiddleware,
+  logoutController
 );
-
 
 export default authRouter;
